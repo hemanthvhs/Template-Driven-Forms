d71c59b0bd6e14fb778bdd3184115aa6045ac09f
hemanthvhs <hemanthvennelakanti@outlook.com>
2021-08-18 09:03:02 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,6 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  valObj = {
-    email: 'hemanthvhs@gmail.com',
-    password: 'test1234'
-  };
-
   login(loginForm: NgForm) {
     console.log(loginForm.value);
   }
